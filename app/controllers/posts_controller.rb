16ac597cae8f320cc50ba6aaac118a973cb04fc5
david-masa <apple.phone.3g.masa@gmail.com>
2020-05-13 18:20:28 +0900

    完了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,10 @@ class PostsController < ApplicationController
   def create
     @post = @genre.posts.new(post_params)
     
+    url = params[:post][:youtube_url]
+    url = url.last(11)
+    @post.youtube_url = url
+
     if @post.save
       redirect_to genre_posts_path(@genre), notice:'メッセージが送信されました'
     else
